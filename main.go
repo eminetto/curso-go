60faa5c0a9af4f59e85576b584372b5b95df3922
Elton Minetto <elton.minetto@betrybe.com>
2022-04-02 14:34:40 -0300

    funcoes

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	hello("mundo")
+	calc(1, 2, 2.5, true)
+}
+
+func hello(word string) {
+	fmt.Printf("\n\tolá %s\n", word)
+}
+
+func calc(a, b int, c float32, d bool) {
+	fmt.Println(a + b)
+	fmt.Println(c)
+	fmt.Println(float64(a) + float64(b) + float64(c))
 }
